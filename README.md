7e4ec0e5ae9a3dc56cfd7daf1604266bacfa52d5
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2019-10-06 16:24:46 -0700

    fixed final bugs and added documentation

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # liri-node-app
+
+Overview: 
+
+    Create an Node App that can recognize a simple set of commands, call out to another server using API/Axios NPM for information, and present it back to the user in the Terminal/bash window as a useful set of data.
+
+Basic Functions/Parameters:
+
+    --Spotify this Song:
+        Command line prompt: node liri.js spotify-this-song <name of song>
+
+        Will return spotify information for song entered in command line, including:
+            Artist:
+            Song:
+            Spotify direct URL:
+            Album:
+![](documentation/spotify_this_works.gif)
+
+         
+
+    --Concert this Artist:
+        Command line prompt: node liri.js concert-this <name of artist>
+
+        Will return next listed concert for artist entered in command line, including:
+            Artist:
+            Description of event:
+            Venue Name:
+            Location:
+            Date:
+
+![](documentation/concert_this_works.gif)
+
+    --Movie info:
+        Command line prompt: node liri.js movie-this <name of movie>
+
+        Will return Open Movie Database information for movie entered in command line, including:
+            Title:
+            Year:
+            Rating:
+            Country of Release:
+            Plot Summary:
+            Actors:
+            Rotten Tomatoes rating:
+
+![](documentation/movie_this_works.gif)
+
+    --Take command from TXT file:
+        From a text file located in the root directory, the app takes in a command(from above) and a title/artist, separated by a comma. Uses command to run same operations as above.
+
+![](documentation/do_what_it_says_works.gif)
+
--- liri.js
+++ liri.js
@@ -2,72 +2,78 @@ require("dotenv").config();
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
-
 var keys = require("./keys.js");
-
 var axios = require("axios");
-
 var spotify = new Spotify(keys.spotify);
+var question = process.argv.slice(3).join(" ");
 
-var question = process.argv.slice(3).join("-");
-
-console.log(question);
+// console.log(question);
 
 switch(process.argv[2]) {
     
     case "concert-this":
+
+        if(question === "") {
+            var question = "String Cheese Incident"
+        }
         function concertThis(){
-        
         var queryURL = "https://rest.bandsintown.com/artists/" + question + "/events?app_id=codingbootcamp";
 
         axios.get(queryURL).then(
             function(response) {
+                console.log("------------------------------------");
+                // console.log(response);
+                console.log(question);
+                console.log("Description: " + response.data[0].description);
                 console.log("Venue: " + response.data[0].venue.name);
                 console.log("City: " + response.data[0].venue.city);
                 console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+                console.log("------------------------------------");
             }
         );
         };
         concertThis();
 
-        //    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-        //    * Name of the venue
-
-        //    * Venue location
-
-        //    * Date of the Event (use moment to format this as "MM/DD/YYYY")
     break;
 
     case "spotify-this-song":
-            function spotifyThisSong() {
+            
             if (question === ""){
                 var question = "the Sign ace of base";
+                console.log(question);
             }
-            spotify.request('https://api.spotify.com/v1/search?q=track:' + question + '&type=track&limit=10', function(error, songResponse) {
+
+            function spotifyThisSong() {
+            spotify.request('https://api.spotify.com/v1/search?q=track:' + question + '&type=track&limit=10').then(function(songResponse) {
                 
-            if (error){
-                    return console.log(error);
-                }
-                console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
-                console.log("Song: " + songResponse.tracks.items[0].name);
-                console.log("URL: " + songResponse.tracks.items[0].preview_url);
-                console.log("Album: " + songResponse.tracks.items[0].album.name);
-            });        
+            console.log("------------------------------------");
+            console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
+            console.log("Song: " + songResponse.tracks.items[0].name);
+            console.log("URL: " + songResponse.tracks.items[0].preview_url);
+            console.log("Album: " + songResponse.tracks.items[0].album.name);
+            console.log("------------------------------------");
+        }
+            );
+             
         };
+        spotifyThisSong();
     break;
 
     case "movie-this":
+         if (question === "") {
+                var question = "Mr Nobody";
+                // console.log(question);
+        };
 
     function movieThis() {
-            if (question === "") {
-                var question = "Mr Nobody";
-        }
+        
             var queryURL = "http://www.omdbapi.com/?t=" + question + "&y=&plot=short&apikey=trilogy";
 
             axios.get(queryURL).then(
             
                 function(movieResponse){
+                    // console.log(movieResponse);
+                    console.log("------------------------------------");
                     console.log("Title: " + movieResponse.data.Title);
                     console.log("Year: " + movieResponse.data.Year);
                     console.log("Rated: " + movieResponse.data.imdbRating);
@@ -75,50 +81,48 @@ switch(process.argv[2]) {
                     console.log("Language: " + movieResponse.data.Language);
                     console.log("Plot: " + movieResponse.data.Plot);
                     console.log("Actors: " + movieResponse.data.Actors);
-                    console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+                    console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[0].Value);
+                    console.log("------------------------------------");
                 }
             );
-            }
+            };
+            movieThis();
     break;
     
-    // case "do-what-it-says":
-    //         function doWhatInfo() {
+    case "do-what-it-says":
+            function doWhatInfo() {
 
-    //             fs.readFile("random.txt", "utf8", function(error, data) {
-    //                 var command;
-    //                 var query;
+                fs.readFile("random.txt", "utf8", function(error, data) {
+                    var command;
+                    var query;
             
-    //                 // If there is a comma, then we will split the string from file in order to differentiate between the command and query
-    //                 // 	--> if there is no comma, then only the command is considered (my-tweets)
-    //                 if(data.indexOf(",") !== -1) {
-    //                     var dataArr = data.split(",");
-    //                     command = dataArr[0];
-    //                     query = dataArr[1];
-    //                 } else {
-    //                     command = data;
-    //                 }
+                    // If there is a comma, then we will split the string from file in order to differentiate between the command and query
+                    // 	--> if there is no comma, then only the command is considered (my-tweets)
+                    if(data.indexOf(",") !== -1) {
+                        var dataArr = data.split(",");
+                        command = dataArr[0];
+                        question = dataArr[1];
+                    } else {
+                        command = data;
+                    };
             
-    //                 // After reading the command from the file, decides which app function to run
-    //                 if(command === "concert-this") {
-    //                     concertThis();
-    //                 } else if(command === "spotify-this-song") {
-    //                     spotifyThisSong(query);
-    //                     console.log(query, command);
-    //                 } else if(command === "movie-this") {
-    //                     movieThis(query);
-    //                 } else { // Use case where the command is not recognized
-    //                     console.log("Command from file is not a valid command! Please try again.")
-    //                 };
+                    // After reading the command from the file, decides which app function to run
+                    if(command === "concert-this") {
+                        concertThis(question);
+                    } else if(command === "spotify-this-song") {
+                        spotifyThisSong(query);
+                        console.log(question, command);
+                    } else if(command === "movie-this") {
+                        movieThis(question);
+                    } else { // Use case where the command is not recognized
+                        console.log("Command from file is not a valid command! Please try again.")
+                    };
                 
-
-    //             doWhatInfo();
+                })
+            };
+                doWhatInfo();
                 
             // * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
-            // * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-       
-            // * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-    break;
-}
-            
-        
+            break;
+        }
